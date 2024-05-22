f871fe15a4edb465b15b5d3d0fa9dbb7dc3bbece
Mohammad Shabib <mohammadshabib3@gmail.com>
2024-05-22 23:30:27 +0300

    Add reply functionally
    Commit Description:
    * add reply functionally when forwarding
    * the message being replied to will be forwarded again and used as a quote

--- source/telegram/Forward.py
+++ source/telegram/Forward.py
@@ -17,36 +17,50 @@ class Forward:
             if event.grouped_id:
                 return
             destination_id = self.forward_config_map.get(event.chat_id).destinationID
-            await self.send_message(destination_id, event.message)
+            reply_message = await self.reply_handler(event.message, destination_id)
+            await self.send_message(destination_id, event.message, reply_to=reply_message)
         except Exception as e:
             print(e)
 
     async def album_handler(self, event):
         try:
             destination_id = self.forward_config_map.get(event.chat_id).destinationID
-            await self.send_album(destination_id, event)
+            reply_message = await self.reply_handler(event.message, destination_id)
+            await self.send_album(destination_id, event, reply_to=reply_message)
         except Exception as e:
             print(e)
 
-    async def send_message(self, destination_channel_id, message):
+    async def reply_handler(self, source_message, destination_id):
+        if not source_message.is_reply:
+            return None
+        replied_message = \
+            (await self.client.get_messages(source_message.chat_id, ids=[source_message.reply_to_msg_id]))[0]
+        replied_message.text = replied_message.text + "```This message was previously sent and is being resent for a reply purpose only```"
+        replied_message_sent = await self.send_message(destination_id, replied_message)
+        return replied_message_sent.id
+
+    async def send_message(self, destination_channel_id, message, reply_to=None):
+        lSent_message = None
         media_path = None
         if message.media:
             media_path = await self.download_media(message)
         text = message.text if message.text else ''
         if media_path:
-            await self.client.send_file(destination_channel_id, media_path, caption=text)
+            lSent_message = await self.client.send_file(destination_channel_id, media_path, caption=text,
+                                                        reply_to=reply_to)
             self.delete_media(media_path)
         else:
-            await self.client.send_message(destination_channel_id, text)
+            lSent_message = await self.client.send_message(destination_channel_id, text, reply_to=reply_to)
+        return lSent_message
 
-    async def send_album(self, destination_channel_id, event):
+    async def send_album(self, destination_channel_id, event, reply_to=None):
         # TODO: ADD compression flag
         media_path_list = []
         for message in event.messages:
             if message.media:
                 media_path = await self.download_media(message)
                 media_path_list.append(media_path)
-        await self.client.send_file(destination_channel_id, media_path_list, caption=event.text)
+        await self.client.send_file(destination_channel_id, media_path_list, caption=event.text, reply_to=reply_to)
         for media_path in media_path_list:
             self.delete_media(media_path)
 
